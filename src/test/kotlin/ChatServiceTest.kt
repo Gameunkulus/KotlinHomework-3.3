da557dac32daa44d8256aadca7e36316fd8fee7d
Gameunkulus <bugr24-8@mail.ru>
2022-07-07 13:47:57 +0300

    programm redacted 2

---
+++ src/test/kotlin/ChatServiceTest.kt
@@ -0,0 +1,34 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ChatServiceTest {
+
+    @Test
+    fun sendMessage() {
+    }
+
+    @Test
+    fun getUnreadChatsCount() {
+    }
+
+    @Test
+    fun checkRead() {
+    }
+
+    @Test
+    fun getChats() {
+    }
+
+    @Test
+    fun getMessageList() {
+    }
+
+    @Test
+    fun deleteMessage() {
+    }
+
+    @Test
+    fun deleteChat() {
+    }
+}
